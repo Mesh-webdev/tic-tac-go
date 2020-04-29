4f314b597e132ff7ce2a2c307b1c1412870e3feb
Meshari <msharisul696@gmail.com>
2020-04-29 10:18:09 +0300

    -- Frontend --
    - Added Player, Game and Colors global objs
    - Implemented create form(nickname, marker and color) with its animation
    - Implemented an option to choose marker with a spinning animation on choosing
    - Implemented createGameRoom function; on game creation. Gets form values, assigns them to Player&Game objs. emits createGame with Player&Game objs to backend
    - Implemented getGeneratedID that gets the uniquely generated id from the backend to show it in the create game form
    - Used ClipboardJS to help with a copy Game ID button
    - Listened to gameCreated and logged game info
    - Implemented a testing function getClients(gameid); emits list event and listens to clints event that returns connected clients to gameid room
    - Implemented join form(nickname, color, gameid) with its animations
    - Implemented joinGameRoom function; On game room joining. Gets form values, assigns them to Player&Game objs. emits startGame with Player&Game objs to backend
    - Listened to gameStarted; initiats tile events(hover, click). gameStarted is only emitted from the backend to the game creater. thus making him play first.
    - Implemented turnPlayed function; takes the clicked tile id. adds it to Player.moves. adds X/Oplayed class. appends X/O Marker element node. Unbinds all events from the tiles. emits turnPlayed event to the backend
    - Listend to yourTurn; allows player to play. calls AddTileEvents function.
    - Implemented addTileEvents function; Adds hover/click events to tiles.
    - Implemented popover helpers for Game ID field on both join game and create game forms
    - Seperated the SVG element node for Marker(X/O) to an obj in Marker.js and imported that to app.js
    - Site title on click redirects back to website(resets).

--- client/app.js
+++ client/app.js
@@ -1,11 +1,54 @@
+// Global variables
+
+// Colors array
+const colors = [
+  {
+    'name': 'red',
+    'hash': '#ff0000',
+  },
+  {
+    'name': 'yellow',
+    'hash': '#ffff00',
+  },
+  {
+    'name': 'blue',
+    'hash': '#0000ff',
+  },
+  {
+    'name': 'purple',
+    'hash': '#800080',
+  },
+];
+// Current turn
+let currentTurn = '';
+
+// is client allowed to play or not
+let allowed = false;
+
+// Player object
+let Player = {
+  nickname: '',
+  marker: '',
+  color: '',
+  moves: [], // this is for keeping track of the tiles ids
+  tilesPlayed: 0, // This for checking win conditions
+  instanceid: '',
+};
+
+// Game object
+let Game = {
+  gameid: '',
+  winComb: [7, 56, 448, 73, 146, 292, 273, 84],
+};
+
+// SVG marker object
+import { Marker } from './marker.js';
+
 const socket = io.connect('http://localhost:8000');
 socket.on('User-connected', (Data) => {
-    console.log(Data.instance);
-})
-
-$(document).ready(() => {
-    $('[data-toggle="popover"]').popover();
-})
+  console.log(`Connected with ID: ${Data.instance}`);
+  Player.instanceid = Data.instance;
+});
 
 // Getting the elements
 
@@ -16,8 +59,8 @@ const createBtn = $('#create-game');
 // --- Elements
 
 // --- --- Top section
-const header = $('.header')
-const nav = $('.navbar')
+const header = $('.header');
+const nav = $('.navbar');
 const mainHeader = $('.main-header');
 const title = $('.title');
 const subTitle = $('.sub-title');
@@ -25,249 +68,471 @@ const action = $('.call-to-action');
 const GO = $('#GO-span');
 
 // --- --- Main section
-const body = $('body')
-const main = $('main')
-const container = $('.container')
-const gameBox = $('.game-box')
+const body = $('body');
+const main = $('main');
+const container = $('.container');
+const gameBox = $('.game-box');
 
-// --- --- Game-box form
-const joinForm = $('.join-form')
-const createForm = $('.create-form')
-const gameID = $('#game-id')
-const joinRoomBtn = $('#join-game-room')
+// --- --- Game-box forms
+const joinForm = $('.join-form');
+const createForm = $('.create-form');
+
+// --- --- --- Join game form
+const joinGameJoinRoomBtn = $('#join-game-room');
+const joinGameNickname = $('#join-game-nickname');
 const joinGameIDInfo = $('.game-id-info');
-const createGameIDInfo = $('.create-game-id-info');
 const joinGameIDInfoSVG = $('#game-id-info-svg');
-const createGameIDInfoSVG = $('#create-game-id-info-svg');
 const joinGameIDPopover = $('#join-game-id-popover');
-const createGameIDPopover = $('#create-game-id-popover');
+const joinGameGameID = $('#join-game-id');
+
+// --- --- --- Create game form
+const createGameCreateRoomBtn = $('#create-game-room');
+const createGameNickname = $('#create-game-nickname');
 const createGameTypeX = $('#type-X');
 const createGameTypeO = $('#type-O');
 const createGameColor = $('.color');
+const createGameIDInfoSVG = $('#create-game-id-info-svg');
+const createGameIDInfo = $('.create-game-id-info');
+const createGameIDPopover = $('#create-game-id-popover');
 const createGameGameIDCopy = $('.copy-game-id');
 const createGameGameIDCopied = $('#create-game-id-copied');
 const createGameGeneratedGameID = $('#generated-game-id');
 
-
-// --- --- Game-box board
+// --- --- --- Game-box board
+const MainBoard = $('.board-main');
 const board = $('.board');
-
-// --- Elements
-
+const boardTitleContainer = $('.board-title');
+const boardTitle = $('#boardTitle');
+const tiles = document.querySelectorAll('.tile');
 
 // Onload hide elements
+// NOTE: betetr make these display: none;
 title.hide();
 subTitle.hide();
 action.hide();
 main.hide();
 joinForm.hide();
 createForm.hide();
-board.hide();
 joinGameIDPopover.hide();
 createGameIDPopover.hide();
 
-
 // Onload animations
 title.fadeIn(600, () => {
-    subTitle.slideDown(300, () => {
-        action.fadeIn(300)
-    })
-})
-
+  subTitle.slideDown(300, () => {
+    action.fadeIn(300);
+  });
+});
 
 // event listeners
 
-// --- Landing elements events
-joinBtn.on('click', joinGame);
-createBtn.on('click', createGame);
+// Landing page elements events
+joinBtn.on('click', displayJoinGameForm);
+createBtn.on('click', displayCreateGameForm);
 
-// --- game-box elements events
-joinRoomBtn.on('click', joinRoom);
+// Game-box page events
+
+// - Create Form events
+
+// --- create game > create
+createGameCreateRoomBtn.on('click', createGameRoom);
 
 // --- create game > player type
 createGameTypeX.on('click', () => {
-    createGameTypeO.removeClass('chosen-type')
-    createGameTypeX.addClass('chosen-type')
-    createGameTypeX.toggleClass('rotate')
-})
+  createGameTypeO.removeClass('chosen-type');
+  createGameTypeX.addClass('chosen-type');
+  createGameTypeX.toggleClass('rotate');
+});
 createGameTypeO.on('click', () => {
-    createGameTypeX.removeClass('chosen-type')
-    createGameTypeO.addClass('chosen-type')
-    createGameTypeO.toggleClass('rotate')
-})
+  createGameTypeX.removeClass('chosen-type');
+  createGameTypeO.addClass('chosen-type');
+  createGameTypeO.toggleClass('rotate');
+});
 // --- create game > player color
 createGameColor.on('click', (e) => {
-    createGameColor.removeClass('chosen-color')
-    $(`.${e.target.classList[1]}`).toggleClass('chosen-color')
-})
-
+  createGameColor.removeClass('chosen-color');
+  $(`.${e.target.classList[1]}`).toggleClass('chosen-color');
+});
 // --- create game > copy ID
 createGameGameIDCopy.on('click', () => {
-    let Clipboard = new ClipboardJS('.copy-game-id');
-    Clipboard.on('success', (e) => {
-        e.clearSelection()
-        createGameGameIDCopied.fadeIn(200, () => {
-            setTimeout(() => {
-                createGameGameIDCopied.fadeOut(100)
-            }, 600)
-        })
-    })
-    Clipboard.on('error', (e) => {
-        console.log(e);
-    })
-})
+  let Clipboard = new ClipboardJS('.copy-game-id');
+  Clipboard.on('success', (e) => {
+    e.clearSelection();
+    createGameGameIDCopied.fadeIn(200, () => {
+      setTimeout(() => {
+        createGameGameIDCopied.fadeOut(100);
+      }, 600);
+    });
+  });
+  Clipboard.on('error', (e) => {
+    console.log(e);
+  });
+});
 
-// --- Info elements events
+// - Join Form events
 
-// Join game > game id info
+// --- join game > join
+joinGameJoinRoomBtn.on('click', joinGameRoom);
+
+// Info elements events
+
+// --- Join game > game id info
 joinGameIDInfo.on('mouseover', () => {
-    joinGameIDPopover.fadeIn(100);
-
-})
+  joinGameIDPopover.fadeIn(100);
+});
 joinGameIDInfo.on('mouseleave', () => {
-    joinGameIDPopover.fadeOut(100);
-})
-// Create game > game id info
+  joinGameIDPopover.fadeOut(100);
+});
+// --- Create game > game id info
 createGameIDInfo.on('mouseover', () => {
-    createGameIDPopover.fadeIn(100);
-
-})
+  createGameIDPopover.fadeIn(100);
+});
 createGameIDInfo.on('mouseleave', () => {
-    createGameIDPopover.fadeOut(100);
-})
-
-
+  createGameIDPopover.fadeOut(100);
+});
 
 title.on('click', () => {
-    window.location = 'http://localhost:8000/'
-})
+  window.location = 'http://localhost:8000/';
+});
 
+// IO events listeners/related functions
 
-// Events
-async function joinGame() {
-    await Animate()
-        .then(() => {
-            animateMain()
-            animateTitle()
-            animateJoinForm()
-        })
-        .catch(() => {
-            // handle err
-        })
+// --- server emits 'gameCreated' when the player succesfully created the game
+socket.on('gameCreated', (data) => {
+  console.log(data);
+});
+
+socket.on('gameStarted', (data) => {
+  // Should add events for hover and click for tiles
+  addTileEvents();
+  console.log(`${data} started`);
+});
+
+// --- server emits 'yourTurn' when current client is allowed to play
+socket.on('yourTurn', (data) => {
+  let playedMarker = Player.marker === 'X' ? 'O' : 'X';
+  let tileid = `tile${data}`;
+
+  // Show the other player marker in the tile he chose
+
+  // add X/Oplayed class
+  $(`#${tileid}`).addClass(`${playedMarker}played`);
+
+  // append X/O svg
+  if (playedMarker === 'X') $(`#${tileid}`).append(Marker.X);
+  else $(`#${tileid}`).append(Marker.O);
+
+  // Add the tile events (allow the current client to play)
+  addTileEvents();
+});
+
+// --- Gets the clients that joined in the given gameid
+function getClients(gameid) {
+  id = `Game-${gameid.toLowerCase()}`;
+
+  socket.emit('list', id);
+
+  socket.on('clients', (clients) => {
+    console.log(`Clients in ${id} :`);
+    console.log(clients);
+  });
 }
 
-async function createGame() {
-
-    await Animate()
-        .then(() => {
-            animateMain()
-            animateTitle()
-            animateCreateForm()
-        })
-        .catch(() => {
-            // handle err
-        })
-
-}
-
-async function joinRoom() {
-    joinForm.fadeOut(100, () => {
-        board.fadeIn(100)
+// Events/Functions
+async function displayCreateGameForm() {
+  // get and assign game id
+  generateGameID();
+  await Animate()
+    .then(() => {
+      animateMain();
+      animateTitle();
+      animateCreateForm();
     })
+    .catch(() => {
+      // handle err
+    });
+}
+
+async function displayJoinGameForm() {
+  await Animate()
+    .then(() => {
+      animateMain();
+      animateTitle();
+      animateJoinForm();
+      board.show();
+    })
+    .catch(() => {
+      // handle err
+    });
+}
+
+async function hideCreateGameForm() {
+  console.log(currentTurn);
+  createForm.fadeOut(300, () => {
+    MainBoard.css('display', 'grid');
+  });
+}
+async function hideJoinGameForm() {
+  joinForm.fadeOut(300, () => {
+    MainBoard.css('display', 'grid');
+  });
+}
+
+function createGameRoom() {
+  // Get player data(nickname, color and marker type)
+  // Should validate the form
+
+  // Nickname
+  Player.nickname = createGameNickname.val();
+
+  // Marker, check if the chosen type is type X; marker = x else marker = o
+  Player.marker =
+    document.querySelector('.chosen-type').id === 'type-X' ? 'X' : 'O';
+
+  // Color
+  colors.forEach((clr) => {
+    if (clr.name === document.querySelector('.chosen-color').classList[1]) {
+      Player.color = clr.hash;
+    }
+  });
+
+  // Set the global variable 'currentTurn' to the selected marker
+  currentTurn = Player.marker.toUpperCase();
+
+  // Emit a create game event
+  socket.emit(
+    'createGame',
+    {
+      Game,
+      Player,
+    },
+    (data) => {
+      if (data.status) {
+        // Hide form and Show board
+        hideCreateGameForm();
+
+        // Show message for player 1
+        boardTitle.text('Waiting for the other player to join');
+
+        // Creat CSS animation then add it to boardTitle.css() here
+        // Also make the board 'disabled'
+      } else {
+        console.log(`Error creating a room: ${data.message}`);
+      }
+    }
+  );
+}
+
+async function joinGameRoom() {
+  // Get and assign player information
+  // Nickname
+  Player.nickname = joinGameNickname.val();
+
+  // Color
+  Player.color = getChosenColor();
+
+  // Player id
+  // Already assigned, see above; on('User-connected') event.
+
+  // Game id
+  Game.gameid = joinGameGameID.val();
+
+  socket.emit(
+    'joinGame',
+    {
+      Game,
+      Player,
+    },
+    (data) => {
+      // If sucessfully joined a game
+      if (data.status) {
+        hideJoinGameForm();
+        console.log(data.marker);
+        Player.marker = data.marker;
+        // Emit a start game event(?)
+        socket.emit('startGame', data.gameid);
+
+        // Change the title to 'Player 1 turn(?)
+      } else {
+        console.log(data);
+      }
+    }
+  );
+}
+
+function getCurrentTurn() {
+  socket.emit('currentTurn', Game);
+}
+
+function getChosenColor() {
+  let hashColor = '';
+  colors.forEach((clr) => {
+    if (clr.name === document.querySelector('.chosen-color').classList[1]) {
+      hashColor = clr.hash;
+    }
+  });
+  return hashColor;
+}
+
+function generateGameID() {
+  // Get and display the game ID from the backend
+  socket.emit('generateID', 'Generating a game ID', (data) => {
+    if (data.status) {
+      Game.gameid = data.id;
+      createGameGeneratedGameID.text(data.id.toUpperCase());
+    }
+  });
 }
 
 function Animate() {
+  return new Promise((resolve, reject) => {
+    //1- Hide centered elements
+    subTitle.fadeOut(100);
+    action.fadeOut(100);
+    title.fadeOut(100, async () => {
+      //2- Move clip path
 
-    return new Promise((resolve, reject) => {
-        //1- Hide centered elements
-        subTitle.fadeOut(100);
-        action.fadeOut(100)
-        title.fadeOut(100, async () => {
-            //2- Move clip path
-
-            // Clip path to cover the full page
-            await animateClip("bottom")
-                .then(async () => {
-                    // clip path to go back up
-                    await animateClip("top")
-                        .then(() => {
-                            // position and size main/game box
-                            main.css({
-                                'position': 'absolute',
-                                'width': '100%',
-                                'top': '9%'
-                            })
-                            gameBox.css('height', '650px')
-                            // Show elements at the top
-                            title.css({
-                                'font-size': '3em',
-                                'font-family': 'Cairo-regular, sans-serif'
-                            });
-                            mainHeader.css({
-                                'top': '29px'
-                            })
-                            // header.css('background', 'none')
-                            body.css('background', 'linear-gradient(180deg, var(--bg1), var(--bg))')
-                            resolve();
-                        })
-                        .catch()
-                })
-                .catch()
-        });
-
-    })
-
-
+      // Clip path to cover the full page
+      await animateClip('bottom')
+        .then(async () => {
+          // clip path to go back up
+          await animateClip('top')
+            .then(() => {
+              // position and size main/game box
+              main.css({
+                'position': 'absolute',
+                'width': '100%',
+                'top': '9%',
+              });
+              gameBox.css('height', '650px');
+              // Show elements at the top
+              title.css({
+                'font-size': '3em',
+                'font-family': 'Cairo-regular, sans-serif',
+              });
+              mainHeader.css({
+                'top': '29px',
+              });
+              // header.css('background', 'none')
+              body.css(
+                'background',
+                'linear-gradient(180deg, var(--bg1), var(--bg))'
+              );
+              resolve();
+            })
+            .catch();
+        })
+        .catch();
+    });
+  });
 }
 
+function addTileEvents() {
+  tiles.forEach((tile) => {
+    if (
+      tile.classList.contains('Xplayed') ||
+      tile.classList.contains('Oplayed')
+    ) {
+      return;
+    } else {
+      // Hover effect
+      $(`#${tile.id}`).on('mouseover', (e) => {
+        $(e.target).addClass(`tileHover${Player.marker}`);
+      });
 
-// MISC animations
+      $(`#${tile.id}`).on('mouseleave', (e) => {
+        $(e.target).removeClass(`tileHover${Player.marker}`);
+      });
+
+      // Click event
+      $(`#${tile.id}`).on('click', (e) => {
+        turnPlayed(e);
+      });
+    }
+  });
+}
+
+function turnPlayed(e) {
+  let tile = parseInt($(e.target).attr('id').slice(4));
+
+  // Add the tile played to the Player moves array
+  Player.moves.push(tile.toString());
+  Player.tilesPlayed += tile;
+  console.log(`Tile ids: `);
+  console.log(Player.moves);
+  console.log('Tiles played');
+  console.log(Player.tilesPlayed);
+
+  // remove the hover class
+  $(e.target).removeClass(`tileHover${Player.marker}`);
+
+  // Add the X/O played class
+  $(e.target).addClass(`${Player.marker}played`);
+
+  // Add the SVG
+  if (Player.marker === 'X') $(e.target).append(Marker.X);
+  else $(e.target).append(Marker.O);
+
+  // Then unbind all events
+  tiles.forEach((tile) => {
+    $(`#${tile.id}`).unbind();
+  });
+
+  // Change the title
+
+  // Communicate with the backend
+  socket.emit('turnPlayed', Player);
+}
+
+// MISC animations/events
 function animateMain() {
-    setTimeout(() => {
-        main.fadeIn(300)
-    }, 300)
+  setTimeout(() => {
+    main.fadeIn(300);
+  }, 300);
 }
 
 function animateTitle() {
-    GO.css('color', 'var(--btn-bg)')
-    title.fadeIn(300);
+  GO.css('color', 'var(--btn-bg)');
+  title.fadeIn(300);
 }
 
 function animateJoinForm() {
-    setTimeout(() => {
-        joinForm.fadeIn(300)
-    }, 300)
+  setTimeout(() => {
+    joinForm.fadeIn(300);
+  }, 300);
 }
 
 function animateCreateForm() {
-    setTimeout(() => {
-        createForm.fadeIn(300)
-    }, 300)
+  setTimeout(() => {
+    createForm.fadeIn(300);
+  }, 300);
 }
 
 function animateClip(direction) {
+  return new Promise((resolve, reject) => {
+    // bottom
+    if (direction === 'bottom') {
+      header.css('transition', 'all 0.7s ease-in-out');
+      header.css('height', '100%');
+      header.css(
+        'clip-path',
+        'polygon(100% 0%, 100% 0px, 100% 100%, 0px 100%, 0px 0px)'
+      );
+      setTimeout(() => {
+        resolve();
+      }, 700);
+    }
 
-    return new Promise((resolve, reject) => {
-        // bottom
-        if (direction === "bottom") {
-            header.css('transition', 'all 0.7s ease-in-out')
-            header.css('height', '100%')
-            header.css('clip-path', 'polygon(100% 0%, 100% 0px, 100% 100%, 0px 100%, 0px 0px)')
-            setTimeout(() => {
-                resolve()
-            }, 700)
-
-        }
-
-        // top
-        if (direction === "top") {
-            header.css('transition', 'all 0.3s ease-in-out')
-            header.css('height', 'var(--header-height)')
-            header.css('clip-path', 'polygon(50% 0%, 100% 0px, 100% 50%, 0px 100%, 0px 0px)')
-            setTimeout(() => {
-                resolve();
-            }, 300)
-        }
-
-    })
+    // top
+    if (direction === 'top') {
+      header.css('transition', 'all 0.3s ease-in-out');
+      header.css('height', 'var(--header-height)');
+      header.css(
+        'clip-path',
+        'polygon(50% 0%, 100% 0px, 100% 50%, 0px 100%, 0px 0px)'
+      );
+      setTimeout(() => {
+        resolve();
+      }, 300);
+    }
+  });
 }
--- server/server.js
+++ server/server.js
@@ -4,18 +4,236 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const crypto = require('crypto');
+
 server.listen(8000, () => {
-    console.log("Listening to port 8000");
-})
+  console.log('Listening to port 8000');
+});
 
 // App middleware
 app.use(express.static('../client'));
 
+// Global variables
+
+// --- Turns counter
+let turns = 0;
+
+// --- Wining coditions array
+let winConditions = [7, 56, 448, 73, 146, 292, 273, 84];
 
 // IO events
 io.on('connection', (socket) => {
-    console.log(socket.id);
-    socket.emit('User-connected', {
-        instance: socket.id
-    })
-})
+  console.log(socket.id);
+  socket.emit('User-connected', {
+    instance: socket.id,
+  });
+
+  // On connection global variables
+  // game object
+  // player1 object
+  // player2 object
+
+  socket.on('createGame', (data, fn) => {
+    let player = data.Player;
+    let game = data.Game;
+
+    gameid = `Game-${game.gameid}`;
+    socket.join(gameid, (err) => {
+      if (err) {
+        console.log(`Unable to create a room, error: ${err}`);
+        fn({
+          status: false,
+          message: err,
+        });
+      } else {
+        io.sockets.adapter.rooms[gameid].player1 = player;
+        console.log(`Create and joined room ${gameid}, Clients:`);
+        console.log(io.sockets.adapter.rooms[gameid].sockets);
+        console.log(`Player 1:`);
+        console.log(io.sockets.adapter.rooms[gameid].player1);
+        io.sockets.in(gameid).emit('gameCreated', `You are in: ${gameid}`);
+        fn({
+          status: true,
+        });
+      }
+    });
+  });
+
+  socket.on('joinGame', (data, fn) => {
+    let player = data.Player;
+    let game = data.Game;
+
+    gameid = `Game-${game.gameid.toLowerCase()}`;
+
+    // Room validation
+
+    // Room is unavailable
+    if (typeof io.sockets.adapter.rooms[gameid] === 'undefined') {
+      console.log(`${gameid} room doesnt exist`);
+      fn({
+        status: false,
+        message: `${gameid} room doesnt exist`,
+      });
+    } else {
+      //Game is available and not full
+      if (io.sockets.adapter.rooms[gameid].length < 2) {
+        socket.join(gameid, (err) => {
+          if (err) {
+            console.log(`Couldn't join, Error ${err}`);
+            fn({
+              status: false,
+              message: err,
+            });
+          } else {
+            _gameid = gameid;
+            io.sockets.adapter.rooms[gameid].player2 = player;
+            console.log(`Joined room ${gameid}, Clients:`);
+            console.log(io.sockets.adapter.rooms[gameid].sockets);
+            console.log(`Player 2`);
+            console.log(io.sockets.adapter.rooms[gameid].player2);
+            io.to(player.instanceid).emit(
+              'gameCreated',
+              `You are in: ${gameid}`
+            );
+            fn({
+              status: true,
+              gameid: gameid,
+              marker: io.sockets.adapter.rooms[gameid].player1.marker === 'X' ?
+                'O' :
+                'X',
+            });
+          }
+        });
+        //Game is available but full
+      } else {
+        console.log(`${gameid} room is full`);
+        fn({
+          status: false,
+          message: `${gameid} room is full`,
+        });
+      }
+    }
+  });
+
+  socket.on('startGame', (data, fn) => {
+    let gameid = data;
+    let player1 = io.sockets.adapter.rooms[gameid].player1;
+    let player2 = io.sockets.adapter.rooms[gameid].player2;
+
+    console.log(`Game started with 2 players`);
+    console.log(`Player 1: `);
+    console.log(player1.instanceid);
+    console.log(`================`);
+    console.log(`Player 2: `);
+    console.log(player2.instanceid);
+
+    io.to(player1.instanceid).emit('gameStarted', gameid);
+  });
+
+  socket.on('turnPlayed', (data) => {
+    console.log(data.tilesPlayed);
+    let tiles = data.tilesPlayed;
+    let moves = data.moves;
+    let lastMove = moves[moves.length - 1];
+
+    // Player 1
+    if (socket.id === io.sockets.adapter.rooms[gameid].player1.instanceid) {
+      turns++;
+      // Check if draw
+
+      io.sockets.adapter.rooms[gameid].player1.moves.push(lastMove);
+      io.sockets.adapter.rooms[gameid].player1.tilesPlayed = tiles;
+      console.log('Player 1 tile IDs: ');
+      console.log(io.sockets.adapter.rooms[gameid].player1.moves);
+      console.log('Player 1 tiles played: ');
+      console.log(io.sockets.adapter.rooms[gameid].player1.tilesPlayed);
+      console.log(`Current turn counter: ${turns}`);
+      console.log('==================================');
+
+      // Check if player 1 has won
+      console.log('Checking Player 1 win condition:');
+      winConditions.forEach((winPosition) => {
+        console.log(
+          `Condition: ${winPosition}. Tiles played: ${io.sockets.adapter.rooms[gameid].player1.tilesPlayed}`
+        );
+        if (
+          (winPosition &
+            io.sockets.adapter.rooms[gameid].player1.tilesPlayed) ==
+          winPosition
+        ) {
+          console.log('Player 1 won!');
+        }
+      });
+    }
+    // Player 2
+    else {
+      turns++;
+      // Check if draw
+
+      io.sockets.adapter.rooms[gameid].player2.moves.push(lastMove);
+      io.sockets.adapter.rooms[gameid].player2.tilesPlayed = tiles;
+      console.log('Player 2 tile IDs: ');
+      console.log(io.sockets.adapter.rooms[gameid].player2.moves);
+      console.log('Player 2 tiles played: ');
+      console.log(io.sockets.adapter.rooms[gameid].player2.tilesPlayed);
+      console.log(`Current turn counter: ${turns}`);
+      console.log('==================================');
+
+      // Check if player 2 has won
+      console.log('Checking Player 2 win condition:');
+      winConditions.forEach((winPosition) => {
+        console.log(
+          `Condition: ${winPosition}. Tiles played: ${io.sockets.adapter.rooms[gameid].player2.tilesPlayed}`
+        );
+        if (
+          (winPosition &
+            io.sockets.adapter.rooms[gameid].player2.tilesPlayed) ==
+          winPosition
+        ) {
+          console.log('Player 2 won!');
+        }
+      });
+    }
+
+    socket.to(gameid).emit('yourTurn', moves[moves.length - 1]);
+  });
+
+  socket.on('generateID', (data, fn) => {
+    crypto.randomBytes(3, (err, buf) => {
+      if (err) {
+        console.log('cant generate an ID: ', err.message);
+        fn({
+          status: false,
+          message: err.message,
+        });
+      }
+
+      fn({
+        status: true,
+        id: buf.toString('hex'),
+      });
+    });
+  });
+
+  socket.on('list', (data) => {
+    io.sockets.in(data).clients((err, cli) => {
+      if (err) console.log(err);
+
+      console.log(cli);
+      socket.emit('clients', cli);
+    });
+  });
+
+  socket.on('currentTurn', (data, fn) => {
+    gameid = `Game-${data.gameid.toLowerCase()}`;
+    console.log(io.sockets.adapter.rooms[gameid].currentTurn);
+
+    fn({
+      status: true,
+      currentTurn: io.sockets.adapter.rooms[gameid].currentTurn,
+    });
+  });
+
+  //
+  //
+}); // End of connection
